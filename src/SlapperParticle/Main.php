eee8d9b0fb0fe37816c7cea6570d485e3b043935
ZenSageX444 <153876401+ZenSageX444@users.noreply.github.com>
2023-12-23 11:45:25 +0700

    Add files via upload

--- src/SlapperParticle/Main.php
+++ src/SlapperParticle/Main.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace SlapperParticle;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+    
+    public function onEnable(): void{
+        $this->getScheduler()->scheduleRepeatingTask(new Effect($this), 2);
+        $this->getLogger()->info("[SlapperParticle] • plugin enable...");
+    }
+}
